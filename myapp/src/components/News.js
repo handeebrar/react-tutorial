134a40b5c3f7e8c93b6bc855b162fdb211c06369
Hande Ebrar GUNESDOGDU <ebrarg@netas.lab.nortel.com>
2018-11-30 14:41:03 +0300

    using props for arrays

--- myapp/src/components/News.js
+++ myapp/src/components/News.js
@@ -3,13 +3,31 @@ import NewsItem from './NewsItem';
 
 class News extends React.Component {
     render(){
-        const title = "react js";
-        const description = "react description";
+        const news = [{
+            title: 'title1',
+            description: 'description1'
+        },
+        {
+            title: 'title2',
+            description: 'description2'
+        },
+        {
+            title: 'title3',
+            description: 'description3'
+        }];
         return(
             <div>
                 <NewsItem
-                    title={title}
-                    description={description}
+                    title={news[0].title}
+                    description={news[0].description}
+                />
+                <NewsItem
+                    title={news[1].title}
+                    description={news[1].description}
+                />
+                <NewsItem
+                    title={news[2].title}
+                    description={news[2].description}
                 />
             </div>
         )
