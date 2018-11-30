6cd2d79b622e881bef460706819dd478303624bb
Hande Ebrar GUNESDOGDU <ebrarg@netas.lab.nortel.com>
2018-11-30 14:19:39 +0300

    component variables added

--- myapp/src/components/News.js
+++ myapp/src/components/News.js
@@ -3,8 +3,10 @@ import NewsItem from './NewsItem';
 
 class News extends React.Component {
     render(){
+        let nameElement = <div>hande</div>
         return(
             <div>
+                { 2+2 === 4 ? nameElement : "hayır"}
                 <NewsItem/>
                 <NewsItem/>
             </div>
