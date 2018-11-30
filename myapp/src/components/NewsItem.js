b73f0f5374fef8e39611353d74cfbede734d2637
Hande Ebrar GUNESDOGDU <ebrarg@netas.lab.nortel.com>
2018-11-30 14:10:48 +0300

    custom defined keyword

--- myapp/src/components/NewsItem.js
+++ myapp/src/components/NewsItem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NewsItemCss from '../NewsItem.css';
 
 class NewsItem extends React.Component {
     render() {
         return (
             <div>
-            <h1>Hello React</h1>
+            <h1 className = 'title'>Hello React</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum congue augue, in dictum neque hendrerit sit amet.</p>
             </div>
         );
