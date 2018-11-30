afc1d0b802c093b704e9a924955c25ba45e0b2f2
Hande Ebrar GUNESDOGDU <ebrarg@netas.lab.nortel.com>
2018-11-30 13:32:07 +0300

    component created

---
+++ myapp/src/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello React</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum congue augue, in dictum neque hendrerit sit amet.</p>
+            </div>
+        );
+    }
+}
+
+export default NewsItem; //componenti disa aktarmak icin kullaniliyor
