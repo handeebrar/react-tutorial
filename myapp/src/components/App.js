b5429fc5b972eaf326980775a2c731103c46c23f
Hande Ebrar GUNESDOGDU <ebrarg@netas.lab.nortel.com>
2018-12-04 09:03:08 +0300

    events and context binding

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -3,29 +3,33 @@ import logo from '../logo.svg';
 import '../App.css';
 import News from './News';
 
+const news = [{
+  id: 1,
+  title: 'title1',
+  description: 'description1'
+},
+{
+  id: 2,
+  title: 'title2',
+  description: 'description2'
+},
+{
+  id: 3,
+  title: 'title3',
+  description: 'description3'
+}];
+
 class App extends Component {
+  addNews(){
+    console.log("addNews function worked.");
+  }
   render() {
-      const news = [{
-        id: 1,
-        title: 'title1',
-        description: 'description1'
-    },
-    {
-        id: 2,
-        title: 'title2',
-        description: 'description2'
-    },
-    {
-        id: 3,
-        title: 'title3',
-        description: 'description3'
-    }];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <News news = {news} name = {"hande"}/>
+        <News news = {news} name = {"hande"} addNews = {this.addNews}/>
       </div>
     );
   }
